072c9aa555e610a57b63969cb4a796c8bf8865d2
Sam <sandeshmas@gmail.com>
2022-07-27 14:40:58 -0700

    After text editor integration

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
 //Imports
-import express from 'express';
-import cors from 'cors';
-import { Server } from 'socket.io';
+import express from "express";
+import cors from "cors";
+import { Server } from "socket.io";
 
 //Global Variables
 const app = express();
 const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => {
-    console.log(`Socket.IO server running at http://localhost:${PORT}/`);
-})
+  console.log(`Socket.IO server running at http://localhost:${PORT}/`);
+});
 const io = new Server(server, {
-    cors: {
-        origin: '*',
-    }
+  cors: {
+    origin: "*",
+  },
 });
 
 //CORS
 app.use(cors());
 
 //Socket Connection
-io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
-    });
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
 });
-
-
